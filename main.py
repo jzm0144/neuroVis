518aee271e93b9a39ae05c0d0580e0eef8d2d3f4
Janzaib Masood <jzm0144@auburn.edu>
2020-02-12 20:11:36 -0600

    Arguments Parsed in the Main File

--- main.py
+++ main.py
@@ -18,3 +18,22 @@ import innvestigate
 from Transformer import Transformer
 from utils import *
 from models import trainModel
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("ageMatchUnmatch", type=str, help= "Enter ageMatched or ageUnmatched")
+parser.add_argument("dataset", type=str, help= "Dataset you want to train on \nADNI, ABIDE, ADHD, PTSD?")
+parser.add_argument("heatmapNumber", type=int, help= "Generate Heatmap for this Example ID")
+parser.add_argument("topPaths", type=int, help= "Most Significant Paths")
+parser.add_argument("label", type=int, help="Generate Heatmap for what class \n Enter class ID")
+
+args = parser.parse_args()
+
+
+print("The Dataset is                                         = ", args.dataset)
+print("The Age                                                = ", args.ageMatchUnmatch)
+print("Generate Heatmap for Data Example                      = ", args.heatmapNumber)
+print("Number of Paths in the Heatmap to be display           = ", args.topPaths)
+print("Clamped Neuron                                         = ", args.label)
+
+
