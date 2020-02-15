038f7ea97b2d57efcba4a3dc95f85e1cedc585e3
Janzaib Masood <jzm0144@auburn.edu>
2020-02-15 05:29:20 -0600

    today tested the clamping and difference between clamping neurons and confimred their working with PTSD experiment

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ import argparse
 import os, sys
 import numpy as np
 import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import seaborn as sb
+
+
 from math import *
+from itertools import combinations
 import ipdb as ipdb
 import keras
 from keras.models import Sequential
@@ -144,6 +150,105 @@ temp = {"ADNI":200, "ABIDE":200,"ADHD":190, "PTSD":125}
 nodeFile = args.dataset + str(temp[args.dataset]) + ".node"
 
 
+# ------------------------------  Seeing the Differences between  ------------------------
+# ------------------------------  PTSD and Control Clamping       ------------------------
+
+for heatmap in heatmaps:
+    #Create the analyzers
+    analyzer = innvestigate.create_analyzer(heatmap[0],
+                                            model_no_softmax,
+                                            neuron_selection_mode = "index",
+                                            **heatmap[1])
+
+    # Generate the heatmaps
+    analysis_ctrl_clamp = analyzer.analyze(inputs, 0)
+    analysis_ptsd_clamp = analyzer.analyze(inputs, 1)
+
+    # Delete the explanations where prediction was incorrect
+    m = []
+    for __ in range(analysis_ctrl_clamp.shape[0]):
+        predNeuron=np.argmax(preds[__]),
+        actualNeuron=np.argmax(outs[__])
+        if predNeuron != actualNeuron:
+            m.append(__)
+            print("skipped = ", __)
+    analysis_ctrl_clamp = np.delete(analysis_ctrl_clamp, m, 0)
+    analysis_ptsd_clamp = np.delete(analysis_ptsd_clamp, m, 0)
+    print(m)
+    del m
+    # Working with 8 Control Subjects
+    controls_with_ctrl_clamp  = analysis_ctrl_clamp[:8,:,:,0]
+    controls_with_ptsd_clamp  = analysis_ptsd_clamp[:8,:,:,0]
+
+
+
+    
+    groupItems = controls_with_ctrl_clamp.shape[0]
+
+    comb8c = list(combinations([_ for _ in range(8)], 2))
+
+    control_ctrl_clamp_Dist = 0
+    control_ptsd_clamp_Dist = 0
+    bwgroupsDist = 0
+    for _ in comb8c:
+        i, j = _
+
+        control_ctrl_clamp_Dist  += np.sqrt(np.square(controls_with_ctrl_clamp[i] - controls_with_ctrl_clamp[j]))
+        control_ptsd_clamp_Dist  += np.sqrt(np.square(controls_with_ptsd_clamp[i] - controls_with_ptsd_clamp[j]))
+        bwgroupsDist             += np.sqrt(np.square(controls_with_ctrl_clamp[i] - controls_with_ptsd_clamp[j]))
+
+    print("Distance Within Controls    = ", np.sum(control_ctrl_clamp_Dist/len(comb8c)))
+    print("Distance Within PTSD        = ", np.sum(control_ptsd_clamp_Dist/len(comb8c)))
+    print("Distance b/w PTSD & Control = ", np.sum(bwgroupsDist/len(comb8c)))
+    
+    plt.figure(1)
+    plt.subplot(331)
+    m1 = sb.heatmap(control_ctrl_clamp_Dist/len(comb8c))
+    plt.subplot(335)
+    m2 = sb.heatmap(control_ptsd_clamp_Dist/len(comb8c))
+    plt.subplot(339)
+    m3 = sb.heatmap(bwgroupsDist/len(comb8c))
+    plt.savefig('dist_control.png')
+    
+    # Working with 8 PTSD Subjects
+    ptsd_with_ctrl_clamp  = analysis_ctrl_clamp[8:16,:,:,0]
+    ptsd_with_ptsd_clamp  = analysis_ptsd_clamp[8:16,:,:,0]
+
+    
+    groupItems = ptsd_with_ctrl_clamp.shape[0]
+
+    comb8c = list(combinations([_ for _ in range(8)], 2))
+
+    ptsd_ctrl_clamp_Dist = 0
+    ptsd_ptsd_clamp_Dist = 0
+    bwgroupsDist = 0
+    for _ in comb8c:
+        i, j = _
+
+        ptsd_ctrl_clamp_Dist  += np.sqrt(np.square(ptsd_with_ctrl_clamp[i] - ptsd_with_ctrl_clamp[j]))
+        ptsd_ptsd_clamp_Dist  += np.sqrt(np.square(ptsd_with_ptsd_clamp[i] - ptsd_with_ptsd_clamp[j]))
+        bwgroupsDist          += np.sqrt(np.square(ptsd_with_ctrl_clamp[i] - ptsd_with_ptsd_clamp[j]))
+
+    print("Distance Within Controls    = ", np.sum(ptsd_ctrl_clamp_Dist/len(comb8c)))
+    print("Distance Within PTSD        = ", np.sum(ptsd_ptsd_clamp_Dist/len(comb8c)))
+    print("Distance b/w PTSD & Control = ", np.sum(bwgroupsDist/len(comb8c)))
+
+    plt.subplot(331)
+    m1 = sb.heatmap(ptsd_ctrl_clamp_Dist/len(comb8c))
+    plt.subplot(335)
+    m2 = sb.heatmap(ptsd_ptsd_clamp_Dist/len(comb8c))
+    plt.subplot(339)
+    m3 = sb.heatmap(bwgroupsDist/len(comb8c))
+    plt.savefig('dist_ptsd.png')
+
+
+
+
+
+
+
+    ipdb.set_trace()
+
 '''
 # ------------------------------  Part 1  ------------------------------------
 # Generate Heatmaps of the Given Example Number
@@ -158,7 +263,6 @@ for heatmap in heatmaps:
     analysis = analyzer.analyze(inputs, args.label)
 
     thisHeatmap = analysis[args.heatmapNumber, :, :, 0]
-
 
     # Save the Heatmap Edge files
     edgeFilePath = saveEdgeFile(thisHeatmap,
@@ -181,7 +285,7 @@ for heatmap in heatmaps:
                  outputPath = 'Results/Part1/'+args.dataset,
                  configFile = 'config.mat')
 
-#ipdb.set_trace()
+    
 
 # ----------------------------------------------------------------------------
 
@@ -219,7 +323,7 @@ for heatmap in heatmaps:
                  edgePath = edge,
                  outputPath = 'Results/Part2a/'+args.dataset,
                  configFile = 'config.mat')
-'''
+
 # ------------------------------  Part 2b  ------------------------------------
 # -------------- Find Most Occurring Paths, given Many Explanations -----------
 # -------------- of the same heatmap method
@@ -273,7 +377,7 @@ for heatmap in heatmaps:
                  configFile = 'config.mat')
 
 
-'''
+
 # ----------------------------------------------------------------------------
 # ------------------------------  Part 3  ------------------------------------
 # -------------- Avg of All Heatmaps For the Same Example --------------------
